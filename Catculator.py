00936f89e4d2734d1e8eea7f43f3b550b9c1403d
TheDevMode <tmode277@gmail.com>
2026-05-18 00:12:46 +0000

    Exit added with history

--- Catculator.py
+++ Catculator.py
@@ -1,37 +1,39 @@
 #Welcome 
 input("\nWELCOME, press enter to start...")
 
-
+while True:
 #Input here
-num_1 = float(input("First number: "))
-num_2 = float(input("Second number: "))
+    num_1 = float(input("First number: "))
+    num_2 = float(input("Second number: "))
 
-#Input operation
-operator = input("Input one ( + , - , x or * , / ): ")
+    #Input operation
+    operator = input("Input one ( + , - , x or * , / ): ")
 
-result = None
+    result = None
 
 #Operation logic
-if operator == "+":
-    result = num_1 + num_2
-elif operator == "-":
-    result = num_1 - num_2
-elif operator == "*" or operator == "x":
-    result = num_1 * num_2
-elif operator == "/":
-    result = num_1 / num_2
-else:
-    result = print("Invalid operation")
+    if operator == "+":
+        result = num_1 + num_2
+    elif operator == "-":
+        result = num_1 - num_2
+    elif operator == "*" or operator == "x":
+        result = num_1 * num_2
+    elif operator == "/":
+        result = num_1 / num_2
+    else:
+        result = print("Invalid operation")
     
 #Easter Egg          
-if result is not None:
-    if result == 63 or result == 63.0:
-        print("Chosen random number!")
+    if result is not None:
+        if result == 63 or result == 63.0:
+            print("Chosen random number!")
         
 
 
 #Logic output
-print(f"{num_1}{operator}{num_2} = {result}",)
+    print(f"{num_1}{operator}{num_2} = {result}",)
 
 #wait for user to exit
-input("\nTask complete! Press Enter to exit...")
+    call = input("\nTask complete! type exit to quit or enter to continue : ")
+    if call == "exit":
+        break 
