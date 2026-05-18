3d13aee452506618ae3fe6a3f67b4ff1b70af0a6
TheDevMode <tmode277@gmail.com>
2026-05-18 09:48:27 +0000

    RejectingWords

--- Catculator.py
+++ Catculator.py
@@ -3,8 +3,13 @@ input("\nWELCOME, press enter to start...")
 
 while True:
 #Input here
-    num_1 = float(input("First number: "))
-    num_2 = float(input("Second number: "))
+    try:
+        num_1 = float(input("First number: "))
+        num_2 = float(input("Second number: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        input("Press Enter to try again...")
+        continue
 
     #Input operation
     operator = input("Input one ( + , - , x or * , / ): ")
